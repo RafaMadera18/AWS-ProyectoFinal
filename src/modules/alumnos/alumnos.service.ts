cf7648dda9c2cbfe9e9b3abe0ceb3e5f1476da55
RafaMadera18 <rafa_madlop@hotmail.com>
2025-11-06 17:52:47 -0600

    feat: Alumno Service

--- src/modules/alumnos/alumnos.service.ts
+++ src/modules/alumnos/alumnos.service.ts
@@ -1,26 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAlumnoDto } from './dto/create-alumno.dto';
 import { UpdateAlumnoDto } from './dto/update-alumno.dto';
+import { Alumno } from './entities/alumno.entity';
 
 @Injectable()
 export class AlumnosService {
+  private readonly alumnos: Alumno[] = [];
+  private idCounter = 1;
+
   create(createAlumnoDto: CreateAlumnoDto) {
-    return 'This action adds a new alumno';
+    const newAlumno: Alumno = {
+      id: this.idCounter++,
+      ...createAlumnoDto,
+    };
+    this.alumnos.push(newAlumno);
   }
 
   findAll() {
-    return `This action returns all alumnos`;
+    return [...this.alumnos];
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} alumno`;
+    return this.alumnos.find((alumno) => alumno.id === id);
   }
 
-  update(id: number, updateAlumnoDto: UpdateAlumnoDto) {
-    return `This action updates a #${id} alumno`;
+  update(id: number, updateAlumnoDto: UpdateAlumnoDto): Alumno {
+    const alumnoIndex = this.alumnos.findIndex((a) => a.id === id);
+    if (alumnoIndex === -1) throw new Error('Alumno not found');
+    this.alumnos[alumnoIndex] = {
+      ...this.alumnos[alumnoIndex],
+      ...updateAlumnoDto,
+    };
+    return this.alumnos[alumnoIndex];
   }
 
   remove(id: number) {
-    return `This action removes a #${id} alumno`;
+    const alumnoIndex = this.alumnos.findIndex((a) => a.id === id);
+    if (alumnoIndex === -1) throw new Error('Alumno not found');
+    this.alumnos.splice(alumnoIndex, 1);
   }
 }
