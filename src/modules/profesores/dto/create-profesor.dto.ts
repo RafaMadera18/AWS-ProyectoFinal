f29758bde5b23b1a3cb476e9583603c2b70cf4af
RafaMadera18 <rafa_madlop@hotmail.com>
2025-11-06 20:15:50 -0600

    feat: Profesor Open API Properties

--- src/modules/profesores/dto/create-profesor.dto.ts
+++ src/modules/profesores/dto/create-profesor.dto.ts
@@ -1,6 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateProfesorDto {
+  @ApiProperty({
+    type: Number,
+    description: 'Número de empleado del profesor',
+  })
   numeroEmpleado: number;
+
+  @ApiProperty({
+    type: String,
+    description: 'Nombres del profesor',
+  })
   nombres: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Apellidos del profesor',
+  })
   apellidos: string;
+
+  @ApiProperty({
+    type: Number,
+    description: 'Número de horas de clase del profesor',
+  })
   horasClase: number;
 }
--- src/modules/profesores/dto/update-profesor.dto.ts
+++ src/modules/profesores/dto/update-profesor.dto.ts
@@ -1,4 +1,29 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProfesorDto } from './create-profesor.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateProfesorDto extends PartialType(CreateProfesorDto) {}
+export class UpdateProfesorDto extends PartialType(CreateProfesorDto) {
+  @ApiProperty({
+    type: Number,
+    description: 'Número de empleado del profesor',
+  })
+  numeroEmpleado: number;
+
+  @ApiProperty({
+    type: String,
+    description: 'Nombres del profesor',
+  })
+  nombres: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Apellidos del profesor',
+  })
+  apellidos: string;
+
+  @ApiProperty({
+    type: Number,
+    description: 'Número de horas de clase del profesor',
+  })
+  horasClase: number;
+}
