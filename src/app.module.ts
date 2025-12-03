b65ad8b45e5105a27c5c62b1998cffc42a73e9a1
RafaMadera18 <rafa_madlop@hotmail.com>
2025-12-03 12:20:00 -0600

    Format & DB conf

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AlumnosModule } from './modules/alumnos/alumnos.module';
 import { ProfesoresModule } from './modules/profesores/profesores.module';
+import { Alumno } from './modules/alumnos/entities/alumno.entity';
+import { Profesor } from './modules/profesores/entities/profesor.entity';
 
 @Module({
-  imports: [AlumnosModule, ProfesoresModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: Number.parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_DATABASE || 'aws_project',
+      entities: [Alumno, Profesor],
+      synchronize: true, // Solo para desarrollo
+    }),
+    AlumnosModule,
+    ProfesoresModule,
+  ],
 })
 export class AppModule {}
