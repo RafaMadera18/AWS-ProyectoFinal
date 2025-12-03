dd98718c1a0f6eb486bb962b92a17401315ab76b
RafaMadera18 <rafa_madlop@hotmail.com>
2025-12-03 15:30:31 -0600

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { AlumnosModule } from './modules/alumnos/alumnos.module';
 import { ProfesoresModule } from './modules/profesores/profesores.module';
-import { Alumno } from './modules/alumnos/entities/alumno.entity';
-import { Profesor } from './modules/profesores/entities/profesor.entity';
 
 @Module({
   imports: [
@@ -13,13 +11,15 @@ import { Profesor } from './modules/profesores/entities/profesor.entity';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: Number.parseInt(process.env.DB_PORT || '5432'),
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || 'postgres',
-      database: process.env.DB_DATABASE || 'aws_project',
-      entities: [Alumno, Profesor],
-      synchronize: true, // Solo para desarrollo
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: { rejectUnauthorized: false },
+      logging: true,
+      autoLoadEntities: true,
+      synchronize: true,
     }),
     AlumnosModule,
     ProfesoresModule,
